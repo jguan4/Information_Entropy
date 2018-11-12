1d6fb99e81ae5924605df07309d52065884b9479
Jiajing Guan <jiajingguan@gmail.com>
2018-11-12 16:41:58 -0500

    update compression_h

--- compression_h.m
+++ compression_h.m
@@ -5,7 +5,7 @@ com_h = zeros(T-fras*n_ahead,1);
 for i = 1:T-fras*n_ahead
     com_hh = zeros(fras, 1);
     for j=0:fras-1
-        A=squeeze(videoData(i+n_ahead*j,:,:));
+        A=squeeze(double(videoData(i+n_ahead*j,:,:)));
         imwrite(A,'newImage.jpg','jpg');
         info = imfinfo('newImage.jpg');
         com_hh(j+1) = info.FileSize;
