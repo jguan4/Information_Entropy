c97c7f8ebd0abce56ec5e42dc59f679eb62b170e
jguan4 <jiajingguan@gmail.com>
2018-11-13 11:24:57 -0500

    update plot file

--- PlotDEMO.m
+++ PlotDEMO.m
@@ -5,29 +5,42 @@ path='E:\JJ Data\New Data\v_const\RawData';
 savepath='E:\JJ Data\New Data\v_const\Result';
 flist=dir(path);
 mnum=size(flist,1)-2;
+time_n_ahead = 1;
+space_n_ahead = 2;
 
-for m=1:2
+a = 1;
+n_average = 4;
+b = ones([1,n_average])/n_average;
+
+for m=1:mnum
     trial_stamp =  flist(m+2).name(1:8);
     time_stamp = flist(m+2).name(9:(length(flist(m+2).name)-4));
-    load(strcat(savepath,'\',trial_stamp,time_stamp, '_VIP.mat'));
-    load(strcat(savepath,'\',trial_stamp,time_stamp, '_space_h.mat'));
-    load(strcat(savepath,'\',trial_stamp,time_stamp, '_time_h.mat'));
+    load(strcat(savepath,'\',trial_stamp,time_stamp, '_VIP_',num2str(1),'.mat'));
+    load(strcat(savepath,'\',trial_stamp,time_stamp, '_space_h_',num2str(space_n_ahead),'.mat'));
+    load(strcat(savepath,'\',trial_stamp,time_stamp, '_time_h_',num2str(time_n_ahead),'.mat'));
+    load(strcat(savepath,'\',trial_stamp,time_stamp, '_com_h_',num2str(1),'.mat'));
     sizeT = length(space_yval);
     sizeP = length(P);
-    P_resize = resample(P,sizeT,sizeP);
+    sizeInd = size(time_h,1);
+%     P_resize = resample(P,sizeT,sizeP);
+    P_filter = filter(b,a,P);
     time_h_ave = squeeze(mean(mean(time_yval,2),3));
     figure
     title(strcat(trial_stamp,time_stamp))
-    subplot(3,1,1)
-    plot(P);
+    subplot(4,1,1)
+    plot(P_filter);
     title('Power')
-    subplot(3,1,2)
+    subplot(4,1,2)
     plot(space_yval);
-    xlim([0 555])
+    xlim([0 sizeInd])
     title('Space h')
-    subplot(3,1,3)
+    subplot(4,1,3)
     plot(time_h_ave);
-    xlim([0 555])
+    xlim([0 sizeInd])
     title('Time h')
+    subplot(4,1,4)
+    plot(com_h);
+    xlim([0 sizeInd])
+    title('Compression h')
     saveas(gcf,strcat(savepath,'/',trial_stamp,time_stamp,'_power_h.fig'))
 end
