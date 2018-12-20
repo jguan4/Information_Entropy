de583cb0c88ec15f75306fe97dc1b306db5f3bd9
Jiajing Guan <jiajingguan@gmail.com>
2018-12-20 14:00:04 -0500

    Update

--- PlotDEMO.m
+++ PlotDEMO.m
@@ -7,13 +7,13 @@ flist=dir(path);
 mnum=size(flist,1)-2;
 time_n_ahead = 2;
 space_n_ahead = 2;
-full = 0;
+full = 1;
 
 a = 1;
 n_average = 8000;
 b = ones([1,n_average])/n_average;
 
-for m=2:mnum
+for m=2
     trial_stamp =  flist(m+2).name(1:8);
     time_stamp = flist(m+2).name(9:(length(flist(m+2).name)-4));
     load(strcat(savepath,'\',trial_stamp,time_stamp, '_VIP.mat'));
@@ -34,21 +34,21 @@ for m=2:mnum
     P_filter = filter(b,a,P);
     time_h_ave = squeeze(mean(mean(time_yval,2),3));
     figure
-    title(strcat(trial_stamp,time_stamp))
+    title(strcat(trial_stamp,time_stamp, ' full=', num2str(full)))
     subplot(4,1,1)
     plot(P_filter);
     title('Power')
     subplot(4,1,2)
     plot(space_yval);
     xlim([0 sizeInd_space])
-    title(strcat('Space h', num2str(space_n_ahead)))
+    title(strcat('Space h_', num2str(space_n_ahead),' full=', num2str(full)))
     subplot(4,1,3)
     plot(time_h_ave);
     xlim([0 sizeInd_time])
-    title(strcat('Time h', num2str(time_n_ahead)))
+    title(strcat('Time h_', num2str(time_n_ahead)))
     subplot(4,1,4)
     plot(com_h);
     xlim([0 sizeInd_space])
-    title(strcat('Compression h', num2str(space_n_ahead)))
+    title(strcat('Compression h_', num2str(space_n_ahead),' full=', num2str(full)))
     saveas(gcf,strcat(savepath,'/',trial_stamp,time_stamp,'_power_h.fig'))
 end
