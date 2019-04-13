94f114fcfbfedbcc3cfc674832271be20d085724
Jiajing Guan <jiajingguan@gmail.com>
2019-04-13 12:14:23 -0400

    update corrdemo

--- CorrDEMO.m
+++ CorrDEMO.m
@@ -4,10 +4,10 @@ format long;
 % Data file path
 % path='E:\JJ Data\New Data\v_const\RawData';
 % savepath='E:\JJ Data\New Data\v_const\Result_new_1';
-% path='E:\JJ Data\New Data\1_24_19\RawData';
-% savepath='E:\JJ Data\New Data\1_24_19\Result';
-path='F:\JJ\3-20-19\small sample steady';
-savepath='F:\JJ\3-20-19\small sample steady result';
+path='E:\JJ Data\New Data\3-20-19\Data';
+savepath='E:\JJ Data\New Data\3-20-19\Result';
+% path='F:\JJ\3-20-19\small sample steady';
+% savepath='F:\JJ\3-20-19\small sample steady result';
 
 flist=dir(path);
 mnum=size(flist,1)-2;
@@ -27,8 +27,9 @@ t_s_corr_lag = [];
 p_s_corr_lag = [];
 p_t_corr_lag = [];
 t_s_corr = [];
-
-for m=1:6
+p_s_corr = [];
+p_t_corr = [];
+for m=1:mnum
     % Loading files
     trial_stamp =  flist(m+2).name(1:8);
     time_stamp = flist(m+2).name(9:(length(flist(m+2).name)-4));
@@ -43,7 +44,7 @@ for m=1:6
     ss = min(sizeInd_time,sizeInd_space);
     P = power_data(1,:);
     P_filter = filter(b,a,P);
-    time_h_ave = zeros(sizeInd_time);
+    time_h_ave = zeros([1,sizeInd_time]);
     for i=1:size(time_yval,1)
         temp = squeeze(time_yval(i,:,:));
         time_h_ave(i) = (sum(sum(temp)))/nnz(temp);
@@ -63,11 +64,13 @@ for m=1:6
     [M,I]=max(abs(t_s_c));
     t_s_corr_lag(arr_ind,:) = [t_s_c(I) t_s_lags(I)];
     
-    [p_s_c, p_s_lags] = xcov(P_resize(2:end),space_yval_resize_n,'coeff');
+    [p_s_c, p_s_lags] = xcov(P_resize,space_yval_resize_n,'coeff');
+    p_s_corr(arr_ind,:) = p_s_c;
     [M,I]=max(abs(p_s_c));
     p_s_corr_lag(arr_ind,:) = [p_s_c(I) p_s_lags(I)];
     
-    [p_t_c, p_t_lags] = xcov(P_resize(2:end),time_h_ave_resize_n,'coeff');
+    [p_t_c, p_t_lags] = xcov(P_resize,time_h_ave_resize_n,'coeff');
+    p_t_corr(arr_ind,:) = p_t_c;
     [M,I]=max(abs(p_t_c));
     p_t_corr_lag(arr_ind,:) = [ p_t_c(I) p_t_lags(I)];
     
@@ -85,18 +88,18 @@ for m=1:6
 %     title('Power vs. Time');
 %     drawnow;
     
-    figure;
-    subplot(3,1,1)
-    plot(t_s_lags,t_s_c);
-    title('Time vs. Space');
-    subplot(3,1,2)
-    plot(p_s_lags,p_s_c);
-    title('Power vs. Space');
-    subplot(3,1,3)
-    plot(p_t_lags,p_t_c);
-    title('Power vs. Time');
+%     figure;
+%     subplot(3,1,1)
+%     plot(t_s_lags,t_s_c);
+%     title('Time vs. Space');
+%     subplot(3,1,2)
+%     plot(p_s_lags,p_s_c);
+%     title('Power vs. Space');
+%     subplot(3,1,3)
+%     plot(p_t_lags,p_t_c);
+%     title('Power vs. Time');
     
-%     
+    
 %     figure(10);
 %     hold on;
 %     caxis([min(P_resize_n) max(P_resize_n)]);
@@ -106,3 +109,19 @@ for m=1:6
 %     title('Time vs. Space')
 %     drawnow;
 end
+
+t_s_c_mean = mean(t_s_corr,1);
+t_s_c_std = std(t_s_corr);
+p_s_c_mean = mean(p_s_corr,1);
+p_s_c_std = std(p_s_corr,1);
+p_t_c_mean = mean(p_t_corr,1);
+p_t_c_std = std(p_t_corr,1);
+figure
+errorbar(-479:479,t_s_c_mean,t_s_c_std);
+title('Time vs. Space')
+figure
+errorbar(-479:479,p_s_c_mean,p_s_c_std);
+title('Power vs. Space')
+figure
+errorbar(-479:479,p_t_c_mean,p_t_c_std);
+title('Power vs. Time')
