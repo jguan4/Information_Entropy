d5598a7df285ceae37bbe7a65d011a9c6787ff87
Jiajing Guan <jiajingguan@gmail.com>
2019-05-14 00:42:35 -0400

    update

--- PlotDEMO.m
+++ PlotDEMO.m
@@ -25,7 +25,10 @@ for m=2
     time_stamp = flist(m+2).name(9:(length(flist(m+2).name)-4));
     load(strcat(savepath,'\',trial_stamp,time_stamp, '_VIP.mat'));
     if full ==1
-        load(strcat(savepath,'\',trial_stamp,time_stamp, '_space_h_vid_',num2str(space_n_ahead),'_',num2str(full),'_test_s.mat'));
+        space1 = load(strcat(savepath,'\',trial_stamp,time_stamp, '_space_h_vid_',num2str(space_n_ahead),'_',num2str(full),'_test_s_transposed.mat'));
+        space_yval_1 = space1.space_yval;
+        space2 = load(strcat(savepath,'\',trial_stamp,time_stamp, '_space_h_vid_',num2str(space_n_ahead),'_',num2str(full),'_test_s.mat'));
+        space_yval_2 = space2.space_yval;
         load(strcat(savepath,'\',trial_stamp,time_stamp, '_time_h_',num2str(time_n_ahead),'_',num2str(full),'_test_s.mat'));
         %         load(strcat(savepath,'\',trial_stamp,time_stamp, '_com_h_vid_',num2str(space_n_ahead),'_',num2str(full),'.mat'));
     else
@@ -33,10 +36,11 @@ for m=2
         load(strcat(savepath,'\',trial_stamp,time_stamp, '_time_h_',num2str(time_n_ahead),'.mat'));
         load(strcat(savepath,'\',trial_stamp,time_stamp, '_com_h_',num2str(space_n_ahead),'_active.mat'));
     end
-    sizeT = length(space_yval);
+    
+    sizeT = length(space_yval_1);
     
     sizeInd_time = size(time_yval,1);
-    sizeInd_space = size(space_yval,1);
+    sizeInd_space = size(space_yval_1,1);
 %     P_resize = resample(P,sizeT,sizeP);
     P = power_data(2,:);
     sizeP = length(P);
@@ -46,20 +50,40 @@ for m=2
         temp = squeeze(time_yval(i,:,:));
         time_h_ave(i) = (sum(sum(temp)))/nnz(temp);
     end
-    space_yval_ave = squeeze(mean(space_yval,2));
+    time_h_ave_resize = resample(time_h_ave,250,sizeInd_time);
+    space_yval_ave_1 = squeeze(mean(space_yval_1,2));
+    space_yval_ave_2 = squeeze(mean(space_yval_2,2));
+    space_yval_ave_add = space_yval_ave_1 + space_yval_ave_2;
+%     time_yval_shift = zeros([1,sizeInd_time]);
+%     time_yval_shift(1:sizeInd_time-20+1) = time_h_ave(20:end);
+    yval_total = space_yval_ave_add;
+    yval_totala = (space_yval_ave_add(11:end-10))/2;
+    yval_totalb = (space_yval_ave_add(11:end-10))/2+time_h_ave_resize';
+    yval_totalc = (space_yval_ave_add(11:end-10))/2-time_h_ave_resize';
+    yval_totald = (space_yval_ave_add(11:end-10))/2-2*time_h_ave_resize';
+
+    %     yval_total = yval_total(1:sizeInd_time-20+1)+time_h_ave(20:end);
     figure
     title(strcat(trial_stamp,time_stamp, ' full=', num2str(full)))
-    subplot(3,1,1)
+    subplot(2,1,1)
     plot(P_filter(n_average+1:end));
     title('Voltage')
-    subplot(3,1,2)
-    plot(space_yval_ave);
-    xlim([0 sizeInd_space])
-    title(strcat('Space h_', num2str(space_n_ahead),' full=', num2str(full)))
-    subplot(3,1,3)
-    plot(time_h_ave);
-    xlim([0 sizeInd_time])
-    title(strcat('Time h_', num2str(time_n_ahead)))
+%     subplot(4,1,2)
+%     plot(space_yval_ave_1);
+%     xlim([0 sizeInd_space])
+%     title(strcat('Space h_', num2str(space_n_ahead),' Row'))
+%     subplot(3,1,2)
+%     plot(space_yval_ave_add);
+%     xlim([0 sizeInd_space])
+%     title(strcat('Space h_', num2str(space_n_ahead),' add'))
+    subplot(2,1,2)
+    plot(detrend(yval_totala));
+    hold all
+    plot(detrend(yval_totalb));
+    plot(detrend(yval_totalc));
+    plot(detrend(yval_totald));
+%     xlim([1 s])
+    title(strcat('Entropy addition'))
     %     subplot(4,1,4)
     %     plot(com_h);
     %     xlim([0 sizeInd_space])
